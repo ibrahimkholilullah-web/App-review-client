9d14db3fdf81613e56975e0b36c1f4a3322e8bda
ibrahimkholilullah-web <nijmehalext@gmail.com>
2025-01-27 13:58:19 +0600

    payment Add

--- src/Components/User/UserInfo/Book.jsx
+++ src/Components/User/UserInfo/Book.jsx
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import userAuth from '../../AuthProvider/userAuth';
 import { Link } from 'react-router-dom';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Book = () => {
     const stripePromise = loadStripe(import.meta.env.VITE_API_STRIPE);
     const { user } = userAuth();
     const [select, setSelect] = useState('');
-
+    const [price, setPrice] = useState(0);
+    useEffect(() => {
+            if (select === "Anti Age Face Treatment") {
+                setPrice(199); // 199 dollars
+            } else if (select === "Hair Color & Styling") {
+                setPrice(99); // 99 dollars
+            } else if (select === "Skin Care Treatment") {
+                setPrice(299); // 299 dollars
+            } else {
+                setPrice(0); // Default price
+            }
+        }, [select]);
+        
+       const handlePaymen = async () =>{
+        const payment = {
+            email: user?.email,
+            price,
+            name: user?.displayName,
+            date: new Date(),
+            status: "pending",
+        };
+        console.log(payment)
+        try{
+          const {data} =   await axios.post(`${import.meta.env.VITE_SERVER_API}/create-payment-success`, payment)
+          if(data?.GatewayURL){
+            window.location.replace(data?.GatewayURL)
+          }
+        }catch (err) {
+            toast.error(err.message)
+        }
+       }
+    
     return (
         <div className="text-[#0C0C0C] p-4 sm:p-6 lg:p-8">
             {/* Header Section */}
@@ -50,7 +83,13 @@ const Book = () => {
                 </div>
 
                 {/* Payment Section */}
-                <div className="mt-6">
+                <div>
+                <h1 className='my-2 font-semibold'>
+                    Your Service charge will be ${price}
+                </h1>
+                    <button onClick={handlePaymen} className='btn bg-[#F73E7B] text-white px-10 my-4 border hover:bg-[#F73E7B] border-black'>Payment</button>
+                </div>
+                {/* <div className="mt-6">
                     <h1 className="text-[#899694] mb-2 text-lg">Pay With</h1>
                     <Elements stripe={stripePromise}>
                         {select ? (
@@ -61,7 +100,7 @@ const Book = () => {
                             </p>
                         )}
                     </Elements>
-                </div>
+                </div> */}
             </div>
         </div>
     );
